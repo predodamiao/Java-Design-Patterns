eb1222694f5b4e5eb92c5bbcd4aaffc785bcea55
Pedro Henrique Ferreira <pedrohfcd@hotmail.com>
2021-06-14 19:53:39 -0300

    Create template

---
+++ src/test/DesignPattern/Template/Template.java
@@ -0,0 +1,12 @@
+package Template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Template {
+    @Test
+    public void templateTest() {
+        assertTrue(true);
+    }
+}
